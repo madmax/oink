47b323d373b3558ebf1a3add1c946d739b1e9894
masukomi <masukomi@masukomi.org>
2020-10-30 15:31:37 -0400

    refactored memory_usage_report into more methods
    could still use some TLC as one method
    still requires 9 params to do its job

--- lib/oink/reports/memory_usage_report.rb
+++ lib/oink/reports/memory_usage_report.rb
@@ -19,49 +19,29 @@ module Oink
 
              # Skip this line since we're only interested in the Hodel 3000 compliant lines
             next unless line =~ HODEL_LOG_FORMAT_REGEX
+            pid = extract_pid_from_line(line, @pids)
 
-            if line =~ /rails\[(\d+)\]/
-              pid = $1
-              @pids[pid] ||= { :buffer => [], :last_memory_reading => -1, :current_memory_reading => -1, :action => "", :request_finished => true }
-              @pids[pid][:buffer] << line
-            end
 
             if line =~ /Oink Action: (([\w\/]+)#(\w+))/
-
-              unless @pids[pid][:request_finished]
-                @pids[pid][:last_memory_reading] = -1
-              end
-              @pids[pid][:action] = $1
-              @pids[pid][:request_finished] = false
+              record_action($1, pid, @pids)
 
             elsif line =~ /Memory usage: (\d+) /
 
               memory_reading = $1.to_i
-              @pids[pid][:current_memory_reading] = memory_reading
+              record_memory_usage(memory_reading, pid, @pids)
 
             elsif line =~ /Oink Log Entry Complete/
 
               oink_entry_count += 1
-              @pids[pid][:request_finished] = true
-              unless @pids[pid][:current_memory_reading] == -1 || @pids[pid][:last_memory_reading] == -1
-                memory_diff = @pids[pid][:current_memory_reading] - @pids[pid][:last_memory_reading]
-                if memory_diff > @threshold
-                  @bad_actions[@pids[pid][:action]] ||= 0
-                  @bad_actions[@pids[pid][:action]] = @bad_actions[@pids[pid][:action]] + 1
-                  date = HODEL_LOG_FORMAT_REGEX.match(line).captures[0]
-                  @bad_requests.push(MemoryOinkedRequest.new(@pids[pid][:action], date, @pids[pid][:buffer], memory_diff))
-                  if @format == :verbose
-                    @pids[pid][:buffer].each { |b| output.puts b }
-                    output.puts "---------------------------------------------------------------------"
-                  end
-                  @bad_actions_averaged[@pids[pid][:action]] ||= []
-                  @bad_actions_averaged[@pids[pid][:action]] << memory_diff
-                end
-              end
-
-              @pids[pid][:buffer] = []
-              @pids[pid][:last_memory_reading] = @pids[pid][:current_memory_reading]
-              @pids[pid][:current_memory_reading] = -1
+              complete_entry(line,
+                             pid,
+                             @pids,
+                             output,
+                             @threshold,
+                             @bad_actions,
+                             @bad_actions_averaged,
+                             @bad_requests,
+                             @format)
 
             end # end elsif
           end # end each_line
@@ -70,6 +50,57 @@ module Oink
         output.puts "---- Oink Entries Parsed: #{oink_entry_count} ----\n" if @format == :verbose
         print_summary(output)
 
+      end
+      private
+      def extract_pid_from_line(line, pids)
+        if line =~ /rails\[(\d+)\]/
+          pid = $1
+          pids[pid] ||= { :buffer => [], :last_memory_reading => -1, :current_memory_reading => -1, :action => "", :request_finished => true }
+          pids[pid][:buffer] << line
+          return pid
+        end
+        nil
+      end
+      def record_action(action, pid, pids)
+        unless pids[pid][:request_finished]
+          pids[pid][:last_memory_reading] = -1
+        end
+        pids[pid][:action] = action
+        pids[pid][:request_finished] = false
+      end
+      def record_memory_usage(memory_reading, pid, pids)
+        pids[pid][:current_memory_reading] = memory_reading
+      end
+      # TODO: refactor to not require a bajillion params
+      def complete_entry(line,
+                         pid,
+                         pids,
+                         output,
+                         threshold,
+                         bad_actions,
+                         bad_actions_averaged,
+                         bad_requests,
+                         format)
+        pids[pid][:request_finished] = true
+        unless pids[pid][:current_memory_reading] == -1 || pids[pid][:last_memory_reading] == -1
+        memory_diff = pids[pid][:current_memory_reading] - pids[pid][:last_memory_reading]
+        if memory_diff > threshold
+          bad_actions[pids[pid][:action]] ||= 0
+          bad_actions[pids[pid][:action]] = bad_actions[pids[pid][:action]] + 1
+          date = HODEL_LOG_FORMAT_REGEX.match(line).captures[0]
+          bad_requests.push(MemoryOinkedRequest.new(pids[pid][:action], date, pids[pid][:buffer], memory_diff))
+          if format == :verbose
+            pids[pid][:buffer].each { |b| output.puts b }
+            output.puts "---------------------------------------------------------------------"
+          end
+          bad_actions_averaged[pids[pid][:action]] ||= []
+          bad_actions_averaged[pids[pid][:action]] << memory_diff
+        end
+        end
+
+        pids[pid][:buffer] = []
+        pids[pid][:last_memory_reading] = pids[pid][:current_memory_reading]
+        pids[pid][:current_memory_reading] = -1
       end
     end
   end
